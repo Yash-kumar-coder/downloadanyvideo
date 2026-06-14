8bd3a47e8feb23ae72ad74965f7b426923feab7e
Yash Kumar <yashkumar62011@gmail.com>
2026-06-15 02:43:14 +0530

    Update API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://downloadanyvideo-backend.onrender.com/api';
 
 export const startDownloadJob = async (url, platform, quality) => {
   const response = await axios.post(`${API_BASE_URL}/jobs`, {
